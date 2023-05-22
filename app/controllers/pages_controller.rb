45c5b0c46d4cbe6ace8f8faaf3e96845f454cd5d
Federico <federico.capaldo@hotmail.com>
2023-05-22 10:28:44 +0100

    added home and contact actions and views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
 
+  def home
+    puts "wassup"
+  end
+
+
   def about
+  end
+
+  def contact
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "pages#home"
 
-  get "about", to: 'pages#about'
+  get "/about", to: "pages#about"
+  get("/contact", to: "pages#contact")
 end
