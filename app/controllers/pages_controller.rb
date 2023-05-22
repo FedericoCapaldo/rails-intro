5311e2766d96420b7568eabe99e4cddbe59e6422
Federico <federico.capaldo@hotmail.com>
2023-05-22 21:00:33 +0100

    show params and link_to helpers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,16 @@ class PagesController < ApplicationController
   end
 
   def contact
+    @members = ["thanh", "dimitri", "germain", "damien", "julien"]
+
+    search = params[:member]
+
+    if search
+        @members = @members.select do |m|
+        puts m
+        m.start_with? search.downcase
+      end
+    end
   end
 
 end
